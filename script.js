31acee0c04abe38d8edee3eef31f34e1b4a99bc9
Marco2007 magnolfi <marcomagno007@gmail.com>
2025-04-22 18:28:37 +0200

    API funzionante

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const cittaInput = document.getElementById("inputCitta");
 const container = document.getElementById("container");
-const citta = document.getElementById("citta");
+const cittaT = document.getElementById("citta");
 const temp = document.getElementById("temp");
-const APIMeteoLink = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=f525b71042eee1c9a21f67159be80fe4&units=metric&lang=it";
-const lat = 0; //latitudine della città
-const lon = 0; //longitudine della città
+const cerca = document.getElementById("cerca");
+const nazione = document.getElementById("nazione");
+const ora = document.getElementById("ora");
+const precipitazioni = document.getElementById("precipitazioni");
+const vento = document.getElementById("vento");
+const condizioni = document.getElementById("condizioni");
+const umidita = document.getElementById("umidita");
+//Nome API weather api
+const APIKey = "433a4377c3ec4e2ca6a123148251004";
+const APIMeteoLink = "http://api.weatherapi.com/v1/current.json?key=433a4377c3ec4e2ca6a123148251004&q=Florence&aqi=no&lang=it";
 
-async function getData() {
+async function getData(citta) {
     
-    const dataRespons = await fetch();
-    const meteoJson = await JSON.stringify(dataRespons);
+    const dataRespons = await fetch(`http://api.weatherapi.com/v1/current.json?key=433a4377c3ec4e2ca6a123148251004&q=${citta}&aqi=no&lang=it`);
+    const meteoJson = await dataRespons.json();
     console.log(meteoJson);
-    
+    temp.innerHTML = meteoJson.current.temp_c + " C";
+    cittaT.innerHTML = meteoJson.location.name;
+    nazione.innerHTML = meteoJson.location.country;
+    ora.innerHTML = meteoJson.location.localtime;
+    precipitazioni.innerHTML = meteoJson.current.precip_in + " %";
+    vento.innerHTML = meteoJson.current.wind_kph + " Km/h";
+    condizioni.innerHTML = meteoJson.current.condition.text;
+    const img = document.createElement("img");
+    img.src = meteoJson.current.condition.icon;
+    condizioni.append(img);
 }
 
-getData();
+
+cerca.addEventListener("click", () => {
+
+    getData(cittaInput.value);
+    cittaInput.value = "";
+    
+});
